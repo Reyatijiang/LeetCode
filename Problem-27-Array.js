ede093511a43695de01cce5ff1465b85c26520c7
Reya Reyatijiang <jiangyedsb@gmail.com>
2025-06-28 14:35:09 -0400

    Create Problem-27-Array.js

---
+++ Problem-27-Array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0;
+    for(let i = 0; i < nums.length;i++){
+
+        if(nums[i] === val){
+            k++;
+            nums.splice(i,1);
+            i--;
+        }
+    }
+    return nums.length; 
+};
